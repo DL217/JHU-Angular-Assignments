1b92694cff221574eab394304c4b1ca589a73b4a
DL217 <74790356+DL217@users.noreply.github.com>
2022-12-21 22:40:05 -0500

    Implemented controller data w/ singleton approach
    Singleton approach corresponds to .service declaration

--- module2_solution/app.js
+++ module2_solution/app.js
@@ -1,5 +1,12 @@
 (function () {
     'use strict';
     angular.module('ShoppingListCheckOff', [])
-
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+    
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    
 })();
